e038ae38c1bad53f4b365713f78df18dd17fa0f4
Tyler Smith <tyler.smith.la@gmail.com>
2018-03-10 13:57:51 -0500

    Building an Add Channel Form with Formik

--- client/src/components/Channels.js
+++ client/src/components/Channels.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Icon } from 'semantic-ui-react';
 
 const ChannelWrapper = styled.div`
   grid-column: 2;
@@ -51,7 +52,7 @@ const user = ({ id, name }) => (
 );
 
 export default ({
-  teamName, username, channels, users,
+  teamName, username, channels, users, onAddChannelClick,
 }) => (
   <ChannelWrapper>
     <PushLeft>
@@ -60,7 +61,10 @@ export default ({
     </PushLeft>
     <div>
       <SideBarList>
-        <SideBarListHeader>Channels</SideBarListHeader>
+        <SideBarListHeader>
+          Channels <Icon onClick={onAddChannelClick} name="add circle" />
+        </SideBarListHeader>
+
         {channels.map(channel)}
       </SideBarList>
     </div>
