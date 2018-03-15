6a6979bdec091988ca96170347db433b3df67ae1
Tyler Smith <tyler.smith.la@gmail.com>
2018-03-14 22:29:57 -0400

    Optimizing Sequelize with Raw Queries

--- client/src/components/Channels.js
+++ client/src/components/Channels.js
@@ -64,6 +64,7 @@ export default ({
   onAddChannelClick,
   teamId,
   onInvitePeopleClick,
+  isOwner,
 }) => (
   <ChannelWrapper>
     <PushLeft>
@@ -73,7 +74,7 @@ export default ({
     <div>
       <SideBarList>
         <SideBarListHeader>
-          Channels <Icon onClick={onAddChannelClick} name="add circle" />
+          Channels {isOwner && <Icon onClick={onAddChannelClick} name="add circle" />}
         </SideBarListHeader>
 
         {channels.map(c => channel(c, teamId))}
@@ -85,10 +86,12 @@ export default ({
         {users.map(user)}
       </SideBarList>
     </div>
-    <div>
-      <a href="#invite-people" onClick={onInvitePeopleClick}>
-        + Invite People
-      </a>
-    </div>
+    {isOwner && (
+      <div>
+        <a href="#invite-people" onClick={onInvitePeopleClick}>
+          + Invite People
+        </a>
+      </div>
+    )}
   </ChannelWrapper>
 );
